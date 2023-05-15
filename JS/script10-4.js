bf10050251d7b2490a88d2714b7088f572a2a766
Frank Wesdijk <frank.wesdijk@gmail.com>
2023-05-15 10:47:01 +0200

    final2

--- JS/script10-4.js
+++ JS/script10-4.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('.product-small-image').click(function(){
        $('.product-large-image').html($(this).html());
-       $('.tekst').html($(this).attr('alt'));        
+       $('.tekst').html($(this).attr('title'));        
 
   })
 
